64082d4ad63fd809980f2546c495bc47e2c235a0
Winny Silva <winny.silva@yahoo.com.br>
2017-05-02 19:50:41 -0300

    teste de animacao e etc

---
+++ WarDinos-Unity/WarDinos/Assets/Link.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour {
+	public int limit =245, it=0,i=0;
+	public float x=0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		while(true){
+			x+=1;
+			if(x>limit){
+				x =0;
+			}
+
+			this.GetComponent<Transform>().position = new Vector3( x,this.GetComponent<Transform>().position.y,this.GetComponent<Transform>().position.z );
+
+		}
+	}
+}
