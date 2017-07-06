9c824b2ed5f292d1593608d628c2b16198a55cdd
Winny Silva <winny.silva@yahoo.com.br>
2017-07-06 08:23:54 -0300

    inicio das conexoes com mongo

---
+++ WarDinos-Unity/WarDinos/Assets/Scripts/DB/ConexaoMongo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using UnityEngine;
+
+public class ConexaoMongo : MonoBehaviour {
+	protected static MongoClient _client;
+	protected static MongoDatabase _database;
+	string host = "mongodb://localhost:27017";
+	void Awake(){
+		
+		_client = new MongoClient();
+		_database =  _client.GetServer().GetDatabase("Wardinos") ; //(host, _client.Settings);
+		var document = new BsonDocument
+		{
+			{ "street", "2 Avenue" },
+			{ "zipcode", "10075" },
+			{ "building", "1480" },
+			{ "coord", new BsonArray { 73.9557413, 40.7720266 } }
+		};
+
+		var collection = _database.GetCollection<BsonDocument>("Logs");
+
+		collection.Insert(document);
+
+	}
+
+	public static MongoCollection getLogCollection(){
+		_client = new MongoClient();
+		_database =  _client.GetServer().GetDatabase("Wardinos") ; //(host, _client.Settings);
+		return _database.GetCollection<BsonDocument>("Logs");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+/*	public static BsonDocument createLog(){
+		
+	}
+*/	public static void writeLog(LoggerMongo log){
+		MongoCollection col = ConexaoMongo.getLogCollection();
+		col.Insert(log);
+	}
+
+
+}
--- WarDinos-Unity/WarDinos/Assets/Scripts/DB/LoggerMongo.cs
+++ WarDinos-Unity/WarDinos/Assets/Scripts/DB/LoggerMongo.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 
 public class LoggerMongo {
-	private int nCena;
-	private string nomeCena;
+	//public Object Id{get;set;}
+	public int nCena {get;set;}
+	public string nomeCena{get;set;}
+	public string idButton{get;set;}
+	public string PlayerNick{get;set;}
+	public int playerID{get;set;}
+	public int buttonPress{get;set;}
+	public string _buttonPress{get;set;}
+	public int dinossauroID{get;set;}
+	public string dinossauroName{get;set;}
+	public string habilidade{get;set;}
+	public int habilidadeID{get;set;}
+	public string acao{get;set;}
 
-
-	public void LogWrite(string msg ){
-		
+	public void writeLog(){
+		ConexaoMongo.writeLog(this);
 	}
 
 
