829d2dcfc50b87cebabce26d0aa2cf1d4ebbb44a
Winny Silva <winny.silva@yahoo.com.br>
2017-07-10 14:51:41 -0300

    faltantes do ultimo commit

---
+++ WarDinos-Unity/WarDinos/Assets/Scripts/Gameplay/PlayerIA.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using System.Windows.Forms;
+
+public class PlayerIA : MonoBehaviour {
+	public GameObject player;
+	public GameObject hud;
+
+	private HUDController playerHudController;
+	private Player playerScr;
+
+
+	void Awake(){
+		playerHudController = hud.GetComponent<HUDController>();
+		playerScr = player.GetComponent<Player>();
+	}
+	// Use this for initialization
+	void Start () {
+		Invoke("Jogar",1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Jogar(){
+		int mv = Random.Range(0,6);
+		Debug.Log(" IA JOGAR");
+	/*	Debug.Log(this.playerHudController.keyUp );
+		Debug.Log(this.playerHudController.keyLeft);
+		Debug.Log(this.playerHudController.keyRight);
+		Debug.Log(this.playerHudController.keyDown );
+		Debug.Log(this.playerHudController.keyConfirm);
+*/
+
+		Event e = Event.KeyboardEvent(""+this.playerHudController.keyRight +"");
+		e.Use();
+	}
+
+}
